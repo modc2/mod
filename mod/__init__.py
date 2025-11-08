36e01a06ba0dbe51746f75aac66c9455bdbea954
Your Name <you@example.com>
2025-11-08 07:40:28 -0500

    main

--- mod/__init__.py
+++ mod/__init__.py
@@ -3,7 +3,7 @@ _mod = Mod()
 for _fn in dir(_mod):
     globals()[_fn] = getattr(_mod, _fn)
 
-def main(self, *args, **kwargs):
+def main( *args, **kwargs):
     """
     Main function to run the mod
     """
