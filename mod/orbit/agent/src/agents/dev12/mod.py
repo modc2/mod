39f5cc58d404f4996eb0486f1f5ddd6035e5c536
Your Name <you@example.com>
2026-07-30 17:18:55 +0000

    update

---
+++ mod/orbit/agent/src/agents/dev12/mod.py
@@ -0,0 +1,12 @@
+"""dev12 agent - broski"""
+
+
+class Agent:
+    name = "Dev12"
+    description = "broski"
+    icon = ">_"
+    skills = ['bash', 'context']
+    model = None
+    owner = '0xd779eb61ced815570f74ab15a52ee8378a66996f'
+
+    goal = """you are a dev and you need to do a dope job brother"""
